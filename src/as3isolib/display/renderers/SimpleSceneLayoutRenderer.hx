bae2ea5c7b5e52095962fb6690789c207680a10b
sergey-miryanov <sergey.miryanov@gmail.com>
2012-07-23 23:27:32 +0600

    #7: Fix SimpleSceneLayoutRenderer
    - sort childrens via 'sorter' functor
    - fix collisionDetectionFunc type

--- src/as3isolib/display/renderers/SimpleSceneLayoutRenderer.hx
+++ src/as3isolib/display/renderers/SimpleSceneLayoutRenderer.hx
@@ -5,34 +5,47 @@
 package as3isolib.display.renderers;
 
 import as3isolib.core.IIsoDisplayObject;
+import as3isolib.core.IIsoContainer;
+import as3isolib.core.IsoDisplayObject;
 import as3isolib.display.scene.IIsoScene;
 
 class SimpleSceneLayoutRenderer implements ISceneLayoutRenderer
 {
-	public var collisionDetection(getCollisionDetection, setCollisionDetection) : Function;
-	public var sortOnProps : Array<Dynamic>;
+	public var collisionDetection(getCollisionDetection, setCollisionDetection) : IsoDisplayObject -> IsoDisplayObject -> Int;
+	public var sorter : IIsoContainer -> IIsoContainer -> Int;
+
+	public function new ()
+	{
+		sorter = null;
+	}
+
 	public function renderScene(scene : IIsoScene) : Void
 	{
-		var children : Array<Dynamic> = scene.displayListChildren.slice();
-		children.sortOn(sortOnProps, Array.NUMERIC);
-		var child : IIsoDisplayObject;
-		var i : UInt;
-		var m : UInt = children.length;
+		var children = scene.displayListChildren;
+		if (sorter != null)
+		{
+			children.sort (sorter);
+		}
+
+		var i : Int = 0;
+		var m : Int = children.length;
 		while(i < m)
 		{
-			child = IIsoDisplayObject(children[i]);
-			if(child.depth != i) scene.setChildIndex(child, i);
+			var child = children[i];
+			if(child.depth != i)
+				scene.setChildIndex(child, i);
+
 			i++;
-		}
-;
+		};
 	}
-	var collisionDetectionFunc : Function;
-	public function getCollisionDetection() : Function
+	var collisionDetectionFunc : IsoDisplayObject -> IsoDisplayObject -> Int;
+	public function getCollisionDetection() : IsoDisplayObject -> IsoDisplayObject -> Int
 	{
 		return collisionDetectionFunc;
 	}
-	public function setCollisionDetection(value : Function) : Function
+	public function setCollisionDetection(value : IsoDisplayObject -> IsoDisplayObject -> Int) : IsoDisplayObject -> IsoDisplayObject -> Int
 	{
 		collisionDetectionFunc = value;
+		return value;
 	}
 }
